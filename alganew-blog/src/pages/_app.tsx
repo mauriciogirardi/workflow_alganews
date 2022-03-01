bfde7c5b98535cb25313087ff918112a74de353c
Mauricio Girardi <maurigirarde@yahoo.com.br>
2022-03-01 00:05:55 -0300

    add progress bar in the App

--- alganew-blog/src/pages/_app.tsx
+++ alganew-blog/src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import "../styles/imports.css";
 import { ThemeProvider } from 'styled-components'
 import type { AppProps as NextAppProps } from 'next/app'
+import ProgressBar from '@badrap/bar-of-progress'
 import Error from 'next/error'
 
 import { GlobalStyles } from 'styles/globalStyles'
@@ -8,12 +9,19 @@ import { Content } from "components/Content";
 import { Header } from "components/Header";
 import { Footer } from "components/Footer";
 import { light } from 'styles/theme'
+import Router from "next/router";
 
 interface CustomAppProps extends NextPageProps { }
 
 type AppProps<P = any> = {
     pageProps: P
 } & Omit<NextAppProps<P>, 'pageProps'>
+
+const progress = new ProgressBar({
+    size: 2,
+    color: light.primaryBackground,
+    delay: 100,
+})
 
 function MyApp({ Component, pageProps }: AppProps<CustomAppProps>) {
     if (pageProps.error) {
@@ -36,5 +44,9 @@ function MyApp({ Component, pageProps }: AppProps<CustomAppProps>) {
         </ThemeProvider>
     )
 }
+
+Router.events.on('routeChangeStart', progress.start)
+Router.events.on('routeChangeComplete', progress.finish)
+Router.events.on('routeChangeError', progress.finish)
 
 export default MyApp

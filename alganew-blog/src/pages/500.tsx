7fcb8bb06ccef1d829199aa63d7e3f9efc5e6218
Mauricio Girardi <maurigirarde@yahoo.com.br>
2022-02-28 23:57:42 -0300

    tratando erro 500

--- alganew-blog/src/pages/500.tsx
+++ alganew-blog/src/pages/500.tsx
@@ -1,29 +1,29 @@
 import Image from 'next/image'
-import * as S from '../styles/NotFoundStyles'
-import notFoundSvg from '../../public/not-found.svg'
-import Link from 'next/link'
 import Head from 'next/head'
+import * as S from '../styles/NotFoundStyles'
+import notInternalErrorSvg from '../../public/internal-error.svg'
+import Link from 'next/link'
 
-export default function NotFound() {
+export default function ServerError() {
     return (
         <>
             <Head>
-                <title>Página não encontrada - 404</title>
+                <title>Erro interno - 500</title>
             </Head>
 
             <S.Wrapper>
                 <Image
-                    src={notFoundSvg}
+                    src={notInternalErrorSvg}
                     width={300}
                     height={300}
                     objectFit="contain"
-                    alt="não encontrado"
+                    alt="erro interno"
                 />
                 <a className='copyright' href="https://storyset.com/web" target="_blank" rel="noreferrer">Web illustrations by Storyset</a>
-                <h2>Página não encontrado!</h2>
-
+                <h2>Erro interno!</h2>
+                <p>O estagiário desconectou um cabo errado...</p>
                 <Link href="/" passHref>
-                    <a>Voltar para  Home</a>
+                    <a>Tentar acessar a Home</a>
                 </Link>
             </S.Wrapper>
         </>

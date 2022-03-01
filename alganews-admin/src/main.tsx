66816fdbc0c3bb5e7cd2c2637fe9388b853f0f8a
Mauricio Girardi <maurigirarde@yahoo.com.br>
2022-03-01 15:08:54 -0300

    Clear project

---
+++ alganews-admin/src/main.tsx
@@ -0,0 +1,11 @@
+import 'antd/dist/antd.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root'),
+);

7e0050d6a375b896464614e29587aef07a93bff2
Mauricio Girardi <maurigirarde@yahoo.com.br>
2022-03-02 10:55:56 -0300

    add config redux

--- alganews-admin/src/main.tsx
+++ alganews-admin/src/main.tsx
@@ -1,13 +1,18 @@
 import 'antd/dist/antd.css';
 import './styles/index.css';
 
+import { Provider } from 'react-redux';
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import App from './App';
+import { store } from './core/store';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );

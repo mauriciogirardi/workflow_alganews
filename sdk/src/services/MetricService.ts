28d9e1c048b19f8211e616338740527c0189e55c
Mauricio Girardi <maurigirarde@yahoo.com.br>
2022-03-02 14:22:24 -0300

    fix name metric to metrics

--- sdk/src/services/MetricService.ts
+++ sdk/src/services/MetricService.ts
@@ -2,41 +2,39 @@ import { Service } from "../Service";
 import { Metric } from "../@types";
 
 class MetricService extends Service {
-    static getTop3Tags() {
-        return this.Http
-            .get<Metric.EditorTagRatio>('/metrics/editor/top3-tags')
-            .then(this.getData)
-    }
+  static getTop3Tags() {
+    return this.Http.get<Metric.EditorTagRatio>(
+      "/metrics/editor/top3-tags"
+    ).then(this.getData);
+  }
 
-    static getEditorMonthlyEarnings() {
-        return this.Http
-            .get<Metric.EditorMonthlyEarnings>('/metrics/editor/monthly-earnings')
-            .then(this.getData)
-    }
+  static getEditorMonthlyEarnings() {
+    return this.Http.get<Metric.EditorMonthlyEarnings>(
+      "/metrics/editor/monthly-earnings"
+    ).then(this.getData);
+  }
 
-    static getMonthlyRevenuesExpenses() {
-        return this.Http
-            .get<Metric.MonthlyRevenuesExpenses>(
-                '/metric/monthly-revenues-expenses',
-                {
-                    headers: {
-                        'Content-Type': 'application/json'
-                    }
-                }
-            ).then(this.getData)
-    }
+  static getMonthlyRevenuesExpenses() {
+    return this.Http.get<Metric.MonthlyRevenuesExpenses>(
+      "/metrics/monthly-revenues-expenses",
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    ).then(this.getData);
+  }
 
-    static getMonthlyRevenuesExpensesChartJs() {
-        return this.Http
-            .get<Metric.MonthlyRevenuesExpensesChartjs>(
-                '/metric/monthly-revenues-expenses',
-                {
-                    headers: {
-                        'Content-Type': 'application/vnd.alganews.charts+json'
-                    }
-                }
-            ).then(this.getData)
-    }
+  static getMonthlyRevenuesExpensesChartJs() {
+    return this.Http.get<Metric.MonthlyRevenuesExpenses>(
+      "/metrics/monthly-revenues-expenses",
+      {
+        headers: {
+          "Content-Type": "application/vnd.alganews.chartjs+json",
+        },
+      }
+    ).then(this.getData);
+  }
 }
 
-export default MetricService
+export default MetricService;

2210f946dc723cdd4d4724da826c754dd4a30077
Mauricio Girardi <maurigirarde@yahoo.com.br>
2022-03-17 16:05:26 -0300

    fix type

--- sdk/src/@types/Payment.ts
+++ sdk/src/@types/Payment.ts
@@ -1,20 +1,22 @@
 import { AlgaNews } from "./AlgaNews";
 
 export namespace Payment {
-    export type Input = AlgaNews.components['schemas']['PaymentInput']
-    export type Detailed = AlgaNews.components['schemas']['PaymentDetailed']
-    export type Summary = AlgaNews.components['schemas']['PaymentSummary']
-    export type Preview = AlgaNews.components['schemas']['PaymentPreview']
-    export type PreviewInput = AlgaNews.components['schemas']['PaymentPreviewInput']
-    export type Paginated = AlgaNews.components['schemas']['PaymentsPaginated']
-    export type PostWithEarnings = AlgaNews.components['schemas']['PostWithEarnings']
+  export type Input = AlgaNews.components["schemas"]["PaymentInput"];
+  export type Detailed = AlgaNews.components["schemas"]["PaymentDetailed"];
+  export type Summary = AlgaNews.components["schemas"]["PaymentSummary"];
+  export type Preview = AlgaNews.components["schemas"]["PaymentPreview"];
+  export type PreviewInput =
+    AlgaNews.components["schemas"]["PaymentPreviewInput"];
+  export type Paginated = AlgaNews.components["schemas"]["PaymentsPaginated"];
+  export type PostWithEarnings =
+    AlgaNews.components["schemas"]["PostWithEarnings"];
 
-    export type Query = {
-        payeeId?: number;
-        payeeEmail?: number;
-        scheduledToMonth?: string;
-        page?: number;
-        size?: number;
-        sort: [keyof Summary, 'asc' | 'desc'];
-    };
+  export type Query = {
+    payeeId?: number;
+    payeeEmail?: number;
+    scheduledToYearMonth?: string;
+    page?: number;
+    size?: number;
+    sort: [keyof Summary, "asc" | "desc"];
+  };
 }
